bdac16889c2cbc4c2a78374aec0a5402670fe5bb
Texas <79846517+Lingz-ui@users.noreply.github.com>
2022-02-07 12:10:26 +0700

    main.js

--- main.js
+++ main.js
@@ -16,10 +16,9 @@ const starts = async (syaa = new WAConnection()) => {
     syaa.logger.level = 'warn'
     syaa.version = [ 2, 2140, 12 ]
     console.log(banner.string)
-    syaa.on('qr', qr => generate(qr, { small: true }))
-
-        /*console.log(color('[','white'), color('!','red'), color(']','white'), color(' Scan bang'))
-    })*/
+    syaa.on('qr', () => {
+        console.log(color('[','white'), color('!','red'), color(']','white'), color(' Scan bang'))
+    })
 
     fs.existsSync('./session.json') && syaa.loadAuthInfo('./session.json')
     syaa.on('connecting', () => {
